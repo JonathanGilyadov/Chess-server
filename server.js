4bcfa6ce7105298c03609ee70b0c71cee6e4dd6c
Jonathan gilyadov <jonathan.gilyadov@gmail.com>
2021-02-19 01:13:08 +0200

    Implemented chat and matchmaking

--- server.js
+++ server.js
@@ -1,90 +1,125 @@
-const { Game, inputGame, getGame } = require('./utils/games');
 const {
-  userJoin,
-  userLeave,
-  getUsers,
-  updateInGameStatus,
-  getLookingToPlayUser,
-  getCurrentUser,
+	Game,
+	inputGame,
+	getGame,
+	createGame,
+	getPendingGame,
+	joinPendingGame,
+} = require('./utils/games');
+const {
+	userJoin,
+	userLeave,
+	getUsers,
+	updateInGameStatus,
+	getLookingToPlayUser,
+	getCurrentUser,
 } = require('./utils/users');
 const { IDGen } = require('./utils/utils');
 
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST'],
-  },
+	cors: {
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST'],
+	},
 });
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+	res.send('<h1>Hello world</h1>');
 });
 
 io.on('connection', (socket) => {
-  console.log('New client connected');
-  socket.on('joinRoom', ({ username }) => {
-    const user = userJoin(socket.id, username);
+	console.log('New client connected');
+	socket.on('joinRoom', ({ username }) => {
+		const user = userJoin(socket.id, username);
 
-    socket.emit('message', 'Welcome');
+		socket.emit('message', 'Welcome');
 
-    socket.broadcast.emit('message', `${user.username} has joined the chat`);
-  });
+		socket.broadcast.emit('message', `${user.username} has joined the chat`);
+	});
 
-  socket.on('message', ({ value, id }) => {
-    io.to(id).emit('chatMessage', value);
-  });
+	socket.on('message', ({ value, id }) => {
+		io.to(id).emit('chatMessage', value);
+	});
 
-  socket.on('lookingToPlay', ({ username }) => {
-    const userLookingToPlay = getLookingToPlayUser();
-    const roomID = IDGen();
+	socket.on('lookingToPlay', ({ username }) => {
+		// Checking to see if there are any pending games
+		const pendingGame = getPendingGame();
 
-    if (!userLookingToPlay) {
-      updateInGameStatus(socket.id, 'LOOKING_TO_PLAY');
+		// Defining user player data here for shorter code
+		const id = socket.id;
+		const thisPlayerData = {
+			username,
+			id,
+		};
 
-      socket.emit('statusSearch', { status: 'LOOKING_FOR_GAME' });
-      socket.join(roomID);
-    } else {
-      const game = new Game(
-        socket.id,
-        username,
-        userLookingToPlay.id,
-        userLookingToPlay.username,
-        roomID
-      );
-      inputGame(game);
-      socket.join(roomID);
-      socket.emit('statusSearch', { status: 'FOUND_GAME', game });
-      socket
-        .to(userLookingToPlay.id)
-        .emit('statusSearch', { status: 'FOUND_GAME', game });
-    }
-  });
+		// Either joining or creating a game
+		if (!pendingGame) {
+			// Creating room id here so socket can join it later
+			const roomID = IDGen();
 
-  socket.on('move', ({ id, fen, move }) => {
-    const game = getGame(id);
-    console.log(game);
-    game.moves.push(move);
-    game.fen = fen;
-    game.turn = game.turn === 'white' ? 'black' : 'white';
-    io.to(id).emit('chatMessage', 'A move was played');
-  });
+			// Updating user status
+			updateInGameStatus(id, 'IN_QUEUE', roomID);
+			createGame(thisPlayerData, null, roomID);
 
-  socket.on('disconnect', () => {
-    console.log('A client has disconnected');
-    const user = userLeave(socket.id);
+			// Sending status back to the client
+			socket.emit('statusSearch', { status: 'LOOKING_FOR_GAME' });
 
-    if (user) {
-      io.emit('message', `${user.username} has left`);
+			// Joinig the room
+			socket.join(roomID);
+		} else {
+			// Getting game id
+			const gameID = pendingGame.id;
 
-      // Send users and room info
-      io.emit('roomUsers', getUsers());
-    }
-  });
+			// Joining pending game
+			// State wise
+			joinPendingGame(thisPlayerData, gameID);
+			// Socket wise
+			socket.join(gameID);
+
+			// Sending status back to the client
+			socket.emit('statusSearch', {
+				status: 'FOUND_GAME',
+				game: pendingGame,
+				message: 'this game from a direct emit',
+			});
+
+			// Sending status to the other client who created the game
+			socket.to(gameID).emit('statusSearch', {
+				status: 'FOUND_GAME',
+				game: pendingGame,
+				message: 'this game from room emit',
+			});
+		}
+	});
+
+	socket.on('move', ({ id, fen, move }) => {
+		const game = getGame(id);
+		console.log(game);
+		game.moves.push(move);
+		game.fen = fen;
+		game.turn = game.turn === 'white' ? 'black' : 'white';
+
+		const message = 'A move as been played';
+
+		io.to(id).emit('chatMessage', { message, move, fen });
+	});
+
+	socket.on('disconnect', () => {
+		console.log('A client has disconnected');
+		const user = userLeave(socket.id);
+
+		if (user) {
+			io.emit('message', `${user.username} has left`);
+
+			// Send users and room info
+			io.emit('roomUsers', getUsers());
+		}
+	});
 });
 
 http.listen(PORT, () => {
-  console.log(`listening on *:${PORT}`);
+	console.log(`listening on *:${PORT}`);
 });
--- utils/games.js
+++ utils/games.js
@@ -1,53 +1,63 @@
-const { IDGEN, decider } = require('./utils');
+const { decider, IDGen } = require('./utils');
+
+// playerData looks like this
+// {
+//   id: PlayerOneID,
+//   username: PlayerOneName,
+//   color: "white" // use color decider to get this
+// }
 
 const games = [];
 
-function Game(
-  PlayerOneID,
-  PlayerOneName,
-  PlayerTwoID,
-  PlayerTwoName,
-  uniqueID = IDGEN()
-) {
-  const colorDecider = decider();
-
-  this.id = uniqueID;
-  this.playerOne = {
-    name: PlayerOneName,
-    id: PlayerOneID,
-    color: colorDecider && true,
-  };
-  this.playerTwo = {
-    name: PlayerTwoName,
-    id: PlayerTwoID,
-    color: colorDecider && false,
-  };
-  this.turn = 'white';
-  this.moves = [];
-  this.fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
-  this.over = false;
+function Game(playerOneData, playerTwoData, uniqueID) {
+	this.id = uniqueID;
+	this.playerOne = playerOneData;
+	this.playerTwo = playerTwoData;
+	this.turn = 'white';
+	this.moves = [];
+	this.fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+	this.over = false;
+	this.pending = true;
 }
-
-const inputGame = (game) => games.push(game);
-
 const getGame = (id) => {
-  const game = games.find((game) => game.id === id);
+	const game = games.find((game) => game.id === id);
 
-  return game;
+	return game;
 };
 
-const updateGame = (id, updatedFields) => {
-  const game = getGame(id);
-  if (!game) return;
+const getPendingGame = () => {
+	const pendingGame = games.find((game) => game.pending);
 
-  const updatedGame = { ...game, ...updatedFields };
+	return pendingGame;
+};
 
-  return updatedGame;
+const joinPendingGame = (playerData, gameID) => {
+	const game = games.find((game) => game.id === gameID);
+
+	game.pending = false;
+	game.playerTwo = playerData;
+};
+
+const createGame = (playerOneData, playerTwoData, uniqueID = IDGen()) => {
+	const newGame = new Game(playerOneData, playerTwoData, uniqueID);
+
+	games.push(newGame);
+};
+
+const updateGameDetails = (id, updatedFields) => {
+	const game = getGame(id);
+	if (!game) return;
+
+	const updatedGame = { ...game, ...updatedFields };
+
+	return updatedGame;
 };
 
 module.exports = {
-  Game,
-  inputGame,
-  getGame,
-  updateGame,
+	Game,
+	getGame,
+	updateGameDetails,
+	createGame,
+	getPendingGame,
+	joinPendingGame,
 };
